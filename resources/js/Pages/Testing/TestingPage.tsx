f9c633b481a89d31796c02364366c7cbd5b33a34
AureliusIvan <aurelius.ivan@student.umn.ac.id>
2023-08-09 09:02:36 +0700

    Update TestingPage.tsx

--- resources/js/Pages/Testing/TestingPage.tsx
+++ resources/js/Pages/Testing/TestingPage.tsx
@@ -1,17 +1,43 @@
 import { PageProps } from '@/types'
-import { Button } from 'antd'
+// import { Button } from 'antd'
+import { EuiButton as Button, EuiFieldText as Input } from '@elastic/eui'
 import React, { useEffect } from 'react'
-// import { useForm } from '@inertiajs/react'
+import { useForm } from '@inertiajs/react'
+import { router } from '@inertiajs/react'
 
+const Card = (webconfig: any) => {
 
-const Card = ({ children }: { children: React.ReactNode }) => {
-    // const { } = useForm()
+    const { data, setData, post, processing, errors } = useForm({
+        value: webconfig.value,
+        remember: false,
+    })
+
+    // submit form 
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        post(route('webconfig.update', webconfig.id))
+    }
+
     return (
-        <div
+        <form
+            onSubmit={handleSubmit}
             className="bg-white overflow-hidden shadow-sm sm:rounded-lg"
         >
-            {children}
-        </div>
+            <div className="p-6 bg-white border-b border-gray-200">
+                <h1 className="text-2xl font-bold">{webconfig.alias}</h1>
+                <Input className="text-gray-500"
+                    defaultValue={webconfig.value}
+                />
+
+                <button
+                    type='submit'
+                    disabled={processing}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
     )
 }
 
@@ -21,29 +47,18 @@ export default function TestingPage({ webconfigs }: PageProps<{ webconfigs: any 
     }, [])
     return (
         <div
+            id="testing-page"
             className="py-12 text-black"
         >
+            {/* <meter value={0.1}></meter> */}
             {
                 webconfigs.map((webconfig: any) => {
                     return (
-                        <Card key={webconfig.id}>
-                            <div className="p-6 bg-white border-b border-gray-200">
-                                <h1 className="text-2xl font-bold">{webconfig.alias}</h1>
-                                <input className="text-gray-500"
-                                    defaultValue={webconfig.value}
-                                />
-                                <Button
-                                    color='black'
-                                    className="ml-2"
-                                    type="dashed"
-                                >
-                                    Save
-                                </Button>
-                            </div>
-                        </Card>
+                        <Card key={webconfig.id}
+                            {...webconfig}
+                        />
                     )
-                }
-                )
+                })
             }
         </div>
     )
