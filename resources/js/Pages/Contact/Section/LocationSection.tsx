5a9b2517ec768f9a2baf355255b8218251d9b205
sshivahh <russelshivah16@gmail.com>
2023-08-14 12:11:14 +0700

    insert embed google maps

--- resources/js/Pages/Contact/Section/LocationSection.tsx
+++ resources/js/Pages/Contact/Section/LocationSection.tsx
@@ -6,6 +6,14 @@ export default function LocationSection() {
     return (
         <div>
             <h2 className='titleLocation'>Lokasi Kami</h2>
+            <div className='containerMap'>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7931.273886064705!2d106.67871147619726!3d-6.311334200000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69e554aa409fab%3A0xe9115f4a97449ef8!2sTaman%20Kafe%20Beth%20Manna!5e0!3m2!1sen!2sid!4v1691976603930!5m2!1sen!2sid"
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade" />
+                <button className='locationButton'>DAPATKAN RUTE LOKASI</button>    
+            </div>
         </div>
     )
 }
