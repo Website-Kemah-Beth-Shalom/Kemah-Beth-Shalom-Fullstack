7517b866024332542024aa14b4584af5493df981
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-08-27 10:56:12 +0700

    revisi responsive footer
    progress 90% tinggal hanya kerapihan overall

--- resources/js/Components/General/Footer.tsx
+++ resources/js/Components/General/Footer.tsx
@@ -8,8 +8,8 @@ import Youtube from "../../../../storage/app/public/sosmed/youtube.svg"
 export default function Footer() {
     return (
         <div className='bg-oldLace text-gunMetal pt-12 w-full'>
-            <div className='bg-white flex w-7/12 mx-auto text-3xl justify-between rounded-full pt-8 pb-8 px-16 ' >                                 
-                <div className='flex gap-12'>
+            <div className='bg-white flex flex-col md:flex-row md:w-7/12 md:mx-auto md:text-3xl justify-between rounded-[30px] md:rounded-full md:pt-8 md:pb-8 md:px-16 mx-12 py-8' >                                 
+                <div className='flex md:gap-12 flex-col md:flex-row mx-auto md:m-0 text-center'>
                     <Link href='/' className={`my-8 `}>
                         Home
                     </Link>
@@ -20,7 +20,7 @@ export default function Footer() {
                         Kontak Kami
                     </Link>
                 </div>
-                <Link href='/donation' className={`my-4 bg-sepia text-white pt-4 pb-4 pr-8 pl-8 rounded-lg items-center `}>
+                <Link href='/donation' className={`md:my-4 bg-sepia text-white pt-4 pb-4 pr-8 pl-8 rounded-lg items-center mx-auto md:m-0`}>
                     Donasi
                 </Link>
             </div>
@@ -29,10 +29,11 @@ export default function Footer() {
                 <img src={Whatsapp} alt="whatsapp logo" className='w-16'/>
                 <img src={Youtube} alt="youtube logo" className='w-16'/>
             </div>
-            <div className='flex justify-center items-center mt-12 gap-12'>
+            <div className='flex flex-col md:flex-row justify-center items-center mt-12 gap-12'>
                 <p>Copyright © Yayasan Shekinah Glory</p>
-                <img src={FooterLogo} alt="Footer"  className='w-80'/>
+                <img src={FooterLogo} alt="Footer"  className='w-80 hidden md:block'/>
                 <p>"Growing In Love Impacting Life"</p>
+                <img src={FooterLogo} alt="Footer"  className='w-80 block md:hidden'/>
             </div>
         </div>
     )
